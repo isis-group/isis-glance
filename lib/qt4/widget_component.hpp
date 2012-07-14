3d6a00ea5750875714e5157f243977b6ff29ef85
Erik Tuerke <tuerke@cbs.mpg.de>
2012-07-14 21:15:09 +0200

    add config for qt4 stuff

---
+++ lib/qt4/widget_component.hpp
@@ -0,0 +1,48 @@
+/****************************************************************
+ *
+ * <Copyright information>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Author: Erik Tuerke, etuerke@googlemail.com
+ *
+ * widget_component.hpp
+ *
+ * Description:
+ *
+ *  Created on: Jul 14, 2012
+ *      Author: tuerke
+ ******************************************************************/
+#ifndef _ISIS_GLANCE_WIDGET_COMPONENT_HPP
+#define _ISIS_GLANCE_WIDGET_COMPONENT_HPP
+
+#include <QWidget>
+#include "util/widget_base.hpp"
+
+namespace isis {
+namespace glance {
+namespace qt4 {
+	
+class WidgetComponent : public QWidget
+{
+	Q_OBJECT
+
+};
+	
+} // end namespace qt4
+} // end namespace glance
+} // end namespace isis
+
+#endif // _ISIS_GLANCE_WIDGET_COMPONENT_HPP
